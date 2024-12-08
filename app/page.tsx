e9696617f8fd66f245d16e517662427b85814b57
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-12-08 18:10:01 +0100

    test
    Signed-off-by: Ruben Claessens <78033666+DrTuup@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex text-center flex-col gap-4 items-center justify-center min-h-screen p-10">
       <h1 className="font-bold text-4xl">Welcome</h1>
       <p>
-        Looks like you&apos;re not logged in yet. Login or create an account to
+        Looks like you are not logged in yet. Login or create an account to
         start logging your progress!
       </p>
       <Button className="w-2/3">Register</Button>
