2fb81b8ff37f43220b24bbda0b8fc052e302338a
Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
2024-12-08 18:17:03 +0100

    landing page (#3)
    * landing page
    
    
    
    ---------
    
    Signed-off-by: Ruben Claessens <78033666+DrTuup@users.noreply.github.com>
    Co-authored-by: Ruben Claessens <rubenclaessens@MacBook-Pro.local>

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex text-center flex-col gap-4 items-center justify-center min-h-screen p-10">
+      <h1 className="font-bold text-4xl">Welcome</h1>
+      <p>
+        Looks like you are not logged in yet. Login or create an account to
+        start logging your progress!
+      </p>
+      <Button className="w-2/3">Register</Button>
+      <Button className="w-2/3">Login</Button>
+    </div>
+  );
 }
